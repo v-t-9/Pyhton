5d713a4e6b3d5b5683ac4bb1426e607748ba97b7
v-t-9 <violeta.a.t.9@gmail.com>
2023-03-31 13:52:46 -0300

    Ejercicios 4 y 5

--- python_unidad4_ej4y5.py
+++ python_unidad4_ej4y5.py
@@ -1,7 +1,23 @@
 # 4)
-# try:
-#     num = int(input("Ingrese un numero divisor: " ,))
-#     print("El resultado es: " , 10/num)
-# except ZeroDivisionError:
-#     print("No se puede dividir por 0. Intente de nuevo")
+# while True:
+#     try:
+#         num = int(input("Ingrese un numero divisor: " ,))
+#         print("El resultado es: " , 10/num)
+#         break
+#     except ZeroDivisionError:
+#         print("No se puede dividir por 0. Intente de nuevo")
+
+
+# 5)
+
+elementos = [1, 5, -2]
+e = -2
+def agregar_una_vez(lista, el):
+        if el in lista:
+            raise ValueError("Error: Imposible añadir elementos duplicados =>" , el)
+        else:
+              lista.append(el)
+              print(lista)
+    
+agregar_una_vez(elementos,e)
 
